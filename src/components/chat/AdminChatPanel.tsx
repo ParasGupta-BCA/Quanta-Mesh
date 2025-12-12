0321091c64fb6d8a7ad11a63c9d04cb9d2c53ca8
Paras Gupta <88033881+ParasGupta-BCA@users.noreply.github.com>
2025-12-12 15:15:15 +0530

    Adjust height of AdminChatPanel component
    Reduce height of AdminChatPanel from 600px to 500px.

--- src/components/chat/AdminChatPanel.tsx
+++ src/components/chat/AdminChatPanel.tsx
@@ -107,7 +107,7 @@ export function AdminChatPanel() {
   }
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 h-[600px] lg:h-[700px]">
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 h-[500px] lg:h-[600px]">
       {/* Conversations List */}
       <Card
         className={`glass-card lg:col-span-1 flex flex-col h-full ${selectedConversation ? 'hidden lg:flex' : 'flex'
